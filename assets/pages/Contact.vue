6efdfff9910e0cf27835e0eff2156b95744678c9
Gemorroj <wapinet@gmail.com>
2018-02-10 16:04:31 +0300

    upd packages

--- assets/pages/Contact.vue
+++ assets/pages/Contact.vue
@@ -4,7 +4,7 @@
 
         <div style="display: flex; align-items: center; height: 100%; width: 100%;">
             <yandex-map :coords="[53.937808, 27.597854]" zoom="16" style="width: 100%; height: 600px;">
-                <ymap-marker marker-type="placemark"
+                <ymap-marker marker-type="placemark" marker-id="1"
                      :coords="[53.937808, 27.597854]"
                      hint-content="Магазин Umberto Imperial"
                      :balloon="{header: 'Umberto Imperial', body: 'Магазин Umberto Imperial, лучшие шмотки в минске', footer: 'тел.: +375 29 123 12 12'}"
