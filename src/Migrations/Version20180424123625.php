4e353700cce00976c9314284c73f1c9ee4b06689
Gemorroj <wapinet@mail.ru>
2018-04-24 15:38:30 +0300

    проставлена уникальность в БД

--- src/Migrations/Version20180424123625.php
+++ src/Migrations/Version20180424123625.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180422202142 extends AbstractMigration
+class Version20180424123625 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
@@ -16,9 +16,12 @@ class Version20180422202142 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
 
         $this->addSql('CREATE TABLE category (id INTEGER NOT NULL, date_create DATETIME NOT NULL, date_update DATETIME DEFAULT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_64C19C15E237E06 ON category (name)');
         $this->addSql('CREATE TABLE photo (id INTEGER NOT NULL, product_id INTEGER UNSIGNED NOT NULL, date_create DATETIME NOT NULL, date_update DATETIME DEFAULT NULL, path VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_14B78418B548B0F ON photo (path)');
         $this->addSql('CREATE INDEX IDX_14B784184584665A ON photo (product_id)');
         $this->addSql('CREATE TABLE product (id INTEGER NOT NULL, category_id INTEGER UNSIGNED NOT NULL, date_create DATETIME NOT NULL, date_update DATETIME DEFAULT NULL, name VARCHAR(255) NOT NULL, description CLOB NOT NULL, price NUMERIC(10, 2) NOT NULL, size VARCHAR(255) DEFAULT NULL, composition VARCHAR(255) DEFAULT NULL, manufacturer VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D34A04AD5E237E06 ON product (name)');
         $this->addSql('CREATE INDEX IDX_D34A04AD12469DE2 ON product (category_id)');
     }
 
