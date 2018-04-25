ffd86f43134da7b37de0b5c129f81359d7d5bc8d
Gemorroj <wapinet@mail.ru>
2018-04-25 23:50:11 +0300

    build fixes

--- webpack.config.js
+++ webpack.config.js
@@ -2,56 +2,63 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const { VueLoaderPlugin } = require('vue-loader');
-const isDevMode = process.env.NODE_ENV !== 'production';
 
-module.exports = {
-    devtool: 'source-map',
-    entry: path.resolve(__dirname + '/assets/app.js'),
-    output: {
-        publicPath: '/build/',
-        filename: "app.js",
-        path: path.resolve(__dirname, "./public/build")
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [{
-                    loader: 'babel-loader'
-                }]
-            },
-            {
-                test: /\.vue$/,
-                use: [{
-                    loader: 'vue-loader'
-                }]
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    query: {
-                        outputPath: 'fonts/'
-                    }
-                }]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    isDevMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
-            }
+module.exports = function (env, argv) {
+    const isDevMode = argv.mode === 'development';
+
+    return {
+        devtool: isDevMode ? 'eval' : 'source-maps',
+        entry: path.resolve(__dirname + '/assets/app.js'),
+        output: {
+            publicPath: '/build/',
+            filename: "app.js",
+            path: path.resolve(__dirname, "./public/build")
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: [{
+                        loader: 'babel-loader'
+                    }]
+                },
+                {
+                    test: /\.vue$/,
+                    use: [{
+                        loader: 'vue-loader'
+                    }]
+                },
+                {
+                    test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+                    use: [{
+                        loader: 'file-loader',
+                        query: {
+                            outputPath: 'fonts/'
+                        }
+                    }]
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        isDevMode ? 'vue-style-loader' : MiniCssExtractPlugin.loader,
+                        'css-loader'
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new VueLoaderPlugin(),
+            new HtmlWebpackPlugin({
+                filename: path.resolve(__dirname, "./public/index.html"),
+                template: path.resolve(__dirname, "./assets/index.html"),
+                xhtml: true,
+                hash: true,
+                inject: "head",
+                minify: isDevMode ? false : true
+            }),
+            new MiniCssExtractPlugin({
+                filename: "app.css?[hash]"
+            })
         ]
-    },
-    plugins: [
-        new VueLoaderPlugin(),
-        new HtmlWebpackPlugin({
-            filename: path.resolve(__dirname, "./public/index.html"),
-            template: path.resolve(__dirname, "./assets/index.html")
-        }),
-        new MiniCssExtractPlugin({
-            filename: "app.css?[hash]"
-        })
-    ]
+    };
 };
