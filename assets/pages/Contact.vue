812160e9101cc3e29314a17684eb78398c5eb53a
Gemorroj <wapinet@mail.ru>
2021-12-22 02:37:10 +0300

    build: upd deps

--- assets/pages/Contact.vue
+++ assets/pages/Contact.vue
@@ -3,9 +3,9 @@
         <p>Contact page</p>
 
         <div style="display: flex; align-items: center; height: 100%; width: 100%;">
-            <yandex-map :coords="[53.937808, 27.597854]" zoom="16" style="width: 100%; height: 600px;">
+            <yandex-map :coords="[53.903017, 27.560780]" zoom="16" style="width: 100%; height: 600px;">
                 <ymap-marker marker-type="placemark" marker-id="1"
-                     :coords="[53.937808, 27.597854]"
+                     :coords="[53.903017, 27.560780]"
                      hint-content="Магазин Umberto Imperial"
                      :balloon="{header: 'Umberto Imperial', body: 'Магазин Umberto Imperial, лучшие шмотки в минске', footer: 'тел.: +375 29 123 12 12'}"
                      :icon="{color: 'green', glyph: 'shopping'}"></ymap-marker>
